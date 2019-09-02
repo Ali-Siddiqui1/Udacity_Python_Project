735303712f64bf6a8a0bb598a488c152d599fe81
Babar Siddiqui <shahrukhsidd577@gmail.com>
2019-09-02 15:37:13 +1000

    added notes

--- media.py
+++ media.py
@@ -1,9 +1,13 @@
+# import library
 import webbrowser
 import fresh_tomatoes
 
+# create movie class
 class Movie():
     #VALID_RATING = ["C", "D","HD"]
+    # a function that takes movie_title, a short movie_storieline, a front fost and a small trailer
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        # define title
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
