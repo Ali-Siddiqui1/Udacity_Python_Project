de55ed50bed4bf084a5d62baff3052b38f447cf0
Babar Siddiqui <shahrukhsidd577@gmail.com>
2019-09-02 15:40:23 +1000

    define a function

--- media.py
+++ media.py
@@ -12,7 +12,7 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
+    # a function that opens a url
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
